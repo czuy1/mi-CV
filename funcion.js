9910097330cc30fbd2f15fcf75762b69f74deab5
gustavo czuhajowkyj <gusczuha@gmail.com>
2024-04-14 22:36:00 -0300

    commit

---
+++ funcion.js
@@ -0,0 +1,9 @@
+function estudio(){
+    location.href="estudios.html";
+}
+function trabajo(){
+    location.href="experiencia.html";
+}
+function phone(){
+    location.href="https://api.whatsapp.com/send?phone=1165076695";
+}
